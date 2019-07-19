f207c93224457905d700616986a260c2daf39e89
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-19 15:48:03 -0500

    created route to create -- not tested

---
+++ controllers/robotController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Robot = require('../models/robot');
+
+router.get('/new', (req,res) => {
+	res.render('new.ejs')
+}
+
+router.put('/:id', (req,res) => {
+	console.log(req.body, 'in put route');
+})
+
+Robot.updateOne({
+	_id: req.params.id 
+},
+	req.body, (err, response) => {
+	if(err){
+		res.send(err);
+	} else{
+		console.log(response);
+		res.redirect('/robots')
+	}
+})
--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const methodOverride = require('method-override');
 // require the express module from the node_modules folder
 // node automatically will look in node_modules for us
 const app = express();
+
+require('./db/db.js')
+
+const robotController = require('./controllers/robotController')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
+app.use('/robots', robotController)
