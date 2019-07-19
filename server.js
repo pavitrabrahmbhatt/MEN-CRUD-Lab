2862180cb2821138dec1d4170b0a67dbb5abd384
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-19 15:30:53 -0500

    Create an express app

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+// require the express module from the node_modules folder
+// node automatically will look in node_modules for us
+const app = express();
