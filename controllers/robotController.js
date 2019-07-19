d3e9086ab01ef502414bf5051bf20fb40e6b7c72
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-19 16:09:18 -0500

    Created a robot

--- controllers/robotController.js
+++ controllers/robotController.js
@@ -4,25 +4,43 @@ const Robot = require('../models/robot');
 
 router.get('/new', (req,res) => {
 	res.render('new.ejs')
+
+})
+
+
+router.post('/', (req, res) => {
+  // contents of the form
+  console.log(req.body)
+//database
+  Robot.create(req.body, (err, createdRobot) => {
+    console.log(createdRobot);
+    if(err){
+      res.send(err);
+    } else {
+      res.redirect('/robots')
+    }
+  });
 })
 
 
 
-router.put('/:id', (req,res) => {
-	console.log(req.body, 'in put route');
 
 
-Robot.updateOne({
-	_id: req.params.id 
-},
-	req.body, (err, response) => {
-	if(err){
-		res.send(err);
-	} else{
-		console.log(response);
-		res.redirect('/robots')
-	}
-})
-})
+// router.put('/:id', (req,res) => {
+// 	console.log(req.body, 'in put route');
+
+
+// Robot.updateOne({
+// 	_id: req.params.id 
+// },
+// 	req.body, (err, response) => {
+// 	if(err){
+// 		res.send(err);
+// 	} else{
+// 		console.log(response);
+// 		res.redirect('/robots')
+// 	}
+// })
+// })
 
 module.exports = router
